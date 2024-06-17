57c27b6490ce74ccf33c09fc65f31704fcc8438a
danilmvv <danilmsnv@gmail.com>
2024-06-17 17:49:37 +0500

    Initial Commit

---
+++ YaToDo/YaToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaToDoApp.swift
+//  YaToDo
+//
+//  Created by Danil Masnaviev on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct YaToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
