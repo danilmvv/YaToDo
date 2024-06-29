394032f85547e9d79512d150bcbfec0aaa2be82d
danilmvv <danilmsnv@gmail.com>
2024-06-29 04:03:45 +0300

    iPad sidebar layout

--- YaToDo/Views/TodoList/TodoList.swift
+++ YaToDo/Views/TodoList/TodoList.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TodoList: View {
     @Environment(ModelData.self) var modelData
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     
     @State private var showAddTodoView = false
     @State private var selectedTodo: TodoItem?
@@ -17,71 +18,111 @@ struct TodoList: View {
     var body: some View {
         @Bindable var modelData = modelData
         
-        NavigationStack {
-            compactSection
-                .toolbar {
-                    ToolbarItem {
-                        Menu {
-                            Picker("Категория", selection: $modelData.filter) {
-                                ForEach(ModelData.FilterCategory.allCases) { category in
-                                    Text(category.rawValue).tag(category)
+        // iPad layout
+        if horizontalSizeClass == .regular && verticalSizeClass == .regular {
+            NavigationSplitView {
+                regularSection
+                    .toolbar {
+                        ToolbarItem {
+                            Menu {
+                                Picker("Категория", selection: $modelData.filter) {
+                                    ForEach(ModelData.FilterCategory.allCases) { category in
+                                        Text(category.rawValue).tag(category)
+                                    }
                                 }
+                                .pickerStyle(.inline)
+                                
+                                Toggle(isOn: $modelData.showCompleted) {
+                                    Text("Показать выполненные")
+                                }
+                            } label: {
+                                Label("Фильтр", systemImage: "ellipsis.circle")
                             }
-                            .pickerStyle(.inline)
-                            
-                            Toggle(isOn: $modelData.showCompleted) {
-                                Text("Показать выполненные")
-                            }
-                        } label: {
-                            Label("Фильтр", systemImage: "ellipsis.circle")
                         }
                     }
+            } detail: {
+                VStack {
+                    Text("<- Выберите тудушку :)")
+                    
+                    Spacer()
+                    
+                    Image("harold")
+                    Text("Когда заработал NavigationSplitView")
+                    
+                    Spacer()
                 }
+            }
+        } else {
+            NavigationStack {
+                compactSection
+                    .toolbar {
+                        ToolbarItem {
+                            Menu {
+                                Picker("Категория", selection: $modelData.filter) {
+                                    ForEach(ModelData.FilterCategory.allCases) { category in
+                                        Text(category.rawValue).tag(category)
+                                    }
+                                }
+                                .pickerStyle(.inline)
+                                
+                                Toggle(isOn: $modelData.showCompleted) {
+                                    Text("Показать выполненные")
+                                }
+                            } label: {
+                                Label("Фильтр", systemImage: "ellipsis.circle")
+                            }
+                        }
+                    }
+            }
         }
     }
     
     private var compactSection: some View {
         ZStack {
             if !modelData.todos.isEmpty {
-                List {
-                    Section {
-                        ForEach(modelData.filteredTodos) { todo in
-                            TodoRow(todo: todo)
-                                .onTapGesture {
-                                    selectedTodo = todo
-                                }
-                                .swipeActions(edge: .leading) {
-                                    Button {
-                                        withAnimation {
-                                            modelData.toggleCompletion(todo)
-                                        }
-                                    } label: {
-                                        Label("Complete", systemImage: "checkmark.circle.fill")
-                                        
-                                    }
-                                    .tint(.green)
-                                }
-                                .swipeActions(edge: .trailing) {
-                                    Button(role: .destructive) {
-                                        modelData.deleteTodo(todo.id)
-                                    } label: {
-                                        Label("Delete", systemImage: "trash.fill")
-                                    }
-                                    
-                                    Button {
+                if !modelData.filteredTodos.isEmpty {
+                    List {
+                        Section {
+                            ForEach(modelData.filteredTodos) { todo in
+                                TodoRow(todo: todo)
+                                    .onTapGesture {
                                         selectedTodo = todo
-                                    } label: {
-                                        Label("Info", systemImage: "info.circle.fill")
                                     }
-                                }
-                                .listRowInsets(EdgeInsets())
+                                    .swipeActions(edge: .leading) {
+                                        Button {
+                                            withAnimation {
+                                                modelData.toggleCompletion(todo)
+                                            }
+                                        } label: {
+                                            Label("Complete", systemImage: "checkmark.circle.fill")
+                                            
+                                        }
+                                        .tint(.green)
+                                    }
+                                    .swipeActions(edge: .trailing) {
+                                        Button(role: .destructive) {
+                                            modelData.deleteTodo(todo.id)
+                                        } label: {
+                                            Label("Delete", systemImage: "trash.fill")
+                                        }
+                                        
+                                        Button {
+                                            selectedTodo = todo
+                                        } label: {
+                                            Label("Info", systemImage: "info.circle.fill")
+                                        }
+                                    }
+                                    .listRowInsets(EdgeInsets())
+                            }
+                        } header: {
+                            Text("Выполнено – \(modelData.todos.filter { $0.isDone }.count)")
                         }
-                    } header: {
-                        Text("Выполнено – \(modelData.todos.filter { $0.isDone }.count)")
                     }
+                    .safeAreaPadding(.bottom, 44)
+                    .animation(.default, value: modelData.filteredTodos)
+                } else {
+                    Text("Все выполнено! :)")
                 }
-                .safeAreaPadding(.bottom, 44)
-                .animation(.default, value: modelData.filteredTodos)
             } else {
                 Text("Добавьте свои дела! :)")
             }
@@ -110,6 +151,54 @@ struct TodoList: View {
             TodoEdit(viewModel: TodoEdit.ViewModel())
         }
     }
+    
+    private var regularSection: some View {
+        ZStack {
+            if !modelData.todos.isEmpty {
+                if !modelData.filteredTodos.isEmpty {
+                    List {
+                        Section {
+                            ForEach(modelData.filteredTodos) { todo in
+                                NavigationLink {
+                                    TodoEdit(viewModel: TodoEdit.ViewModel(todo: todo))
+                                        .id(todo.id)
+                                } label: {
+                                    TodoRow(todo: todo)
+                                }
+                            }
+                        } header: {
+                            Text("Выполнено – \(modelData.todos.filter { $0.isDone }.count)")
+                        }
+                    }
+                    .safeAreaPadding(.bottom, 44)
+                    .animation(.default, value: modelData.filteredTodos)
+                } else {
+                    Text("Все выполнено :)")
+                }
+            } else {
+                Text("Добавьте свои дела! :)")
+            }
+            
+            VStack {
+                Spacer()
+                NavigationLink {
+                    TodoEdit(viewModel: TodoEdit.ViewModel())
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44)
+                        .background(.white)
+                        .clipShape(Circle())
+                        .shadow(radius: 8)
+                        .foregroundStyle(.accent)
+                }
+            }
+        }
+        .navigationTitle("Мои Дела")
+    }
+    
+    
 }
 
 #Preview {
